700529b883146285de1b9ae6ea20b383a967960d
Nikita <nviliums0@gmail.com>
2020-01-07 21:56:56 +0300

    Update gg.py V0.1
    Добавил ввод логина

--- gg.py
+++ gg.py
@@ -1,10 +1,7 @@
 import random
 
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-<<<<<<< HEAD
 username = input("Введите логин от аккаунта: ")
-=======
->>>>>>> 44e6af2333877f8f0ec17997832711a925b6923f
 name = input("Введите для какого сервиса вам нужен пароль: ")
 password = ""
 for x in range(8, 23):
@@ -12,10 +9,5 @@ for x in range(8, 23):
 print("\n[ "+ name + " ]" + "\nЛОГИН: " + username + "\nПАРОЛЬ: " + password)
 #Создание текстового файла и запись пароля
 file = open('pass.txt', 'a')
-<<<<<<< HEAD
 file.write("\n[ "+ name + " ]" + "\nЛОГИН: " + username + "\nПАРОЛЬ: " + password)
 file.close()
-=======
-file.write("\nВаш парооль от " + name + ": " + password)
-file.close()
->>>>>>> 44e6af2333877f8f0ec17997832711a925b6923f
