44e6af2333877f8f0ec17997832711a925b6923f
Nikita <58393897+talashov@users.noreply.github.com>
2020-01-07 20:27:27 +0300

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,7 +1,7 @@
 import random
 
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-name = input("Введите для чего какого сервиса вам нужен пароль: ")
+name = input("Введите для какого сервиса вам нужен пароль: ")
 password = ""
 for x in range(8, 23):
     password = password + random.choice(chars)
