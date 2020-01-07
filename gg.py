e71e0fa763d0802b04cefed144ec2aa713f25dc1
Nikita <nviliums0@gmail.com>
2020-01-07 19:03:03 +0300

    Обновил код
    Ничего нового не добавил

--- gg.py
+++ gg.py
@@ -1,16 +1,9 @@
-# -*- coding:utf -8 -*-
-#!/usr/bin/python3
-
 import random
 
-##chars = '+ '
-number = input('количество паролей?'+ "\n")
-length = input('длина пароля?'+ "\n")
-number = int(number)
-length = int(length)
-for n in range(number):
-    password =''
-    for i in range(length):
-        password += random.choice(chars)
-    print(password)
-    input('Press ENTER to exit')
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+name = input("Введите для чего какого сервиса вам нужен пароль: ")
+password = ""
+for x in range(8, 23):
+    password = password + random.choice(chars)
+print("Ваш парооль от " + name + ": " + password)
+input("Нажми Enter для выхода")
